822881006ce593a921bc1af7d22744595f95212b
Kenny Tran <trke1011@hs-karlsruhe.de>
2020-11-06 00:32:09 +0100

    stepper used and customized

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule],
-  exports: [MatFormFieldModule, MatSelectModule, MatInputModule],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
