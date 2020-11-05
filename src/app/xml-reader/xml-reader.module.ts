c2f99c9c7a58bd16a11abe664e3d599a5964c6ec
Kenny Tran <trke1011@hs-karlsruhe.de>
2020-11-05 18:33:34 +0100

    read xml and convert to js as a service

--- src/app/xml-reader/xml-reader.module.ts
+++ src/app/xml-reader/xml-reader.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { XmlReaderComponent } from './xml-reader.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [XmlReaderComponent],
+  imports: [CommonModule],
+  exports: [XmlReaderComponent],
 })
-export class XmlReaderModule { }
+export class XmlReaderModule {}
