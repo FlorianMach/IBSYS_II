2bc4061554dd02a8afc75cc7a682b9eb2fc244ad
wosi1018 <55805128+wosi1018@users.noreply.github.com>
2020-11-17 21:55:06 +0100

    created tabs + tables for result data

--- src/app/result/result.module.ts
+++ src/app/result/result.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultComponent } from './result.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [ResultComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatTableModule
+  ],
   exports: [ResultComponent],
 })
 export class ResultModule {}
