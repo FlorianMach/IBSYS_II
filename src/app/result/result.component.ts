fcf78690c1559de660ba075c1e1dbf0f0a3d1a0d
wosi1018 <55805128+wosi1018@users.noreply.github.com>
2020-11-18 16:36:12 +0100

    Revert "created tabs + tables for result data"
    This reverts commit 8e92e8e94a2d3a93a41f8119c17321cac2560390.

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -2,30 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { XmlReaderService } from '../xml-reader/xml-reader.service';
 import { ResultService } from './result.service';
 
-export interface ProdCapacities {
-  workplace: string;
-  shifts: number;
-  overtime: number;
-}
-
-const CAPACITIES: ProdCapacities[] = [
-  {workplace: '1', shifts: 1, overtime: 10},
-  {workplace: '2', shifts: 1, overtime: 20},
-  {workplace: '3', shifts: 1, overtime: 30},
-  {workplace: '4', shifts: 1, overtime: 40},
-  {workplace: '5', shifts: 0, overtime: 0},
-  {workplace: '6', shifts: 1, overtime: 60},
-  {workplace: '7', shifts: 2, overtime: 70},
-  {workplace: '8', shifts: 2, overtime: 80},
-  {workplace: '9', shifts: 2, overtime: 90},
-  {workplace: '10', shifts: 1, overtime: 100},
-  {workplace: '11', shifts: 1, overtime: 110},
-  {workplace: '12', shifts: 1, overtime: 120},
-  {workplace: '13', shifts: 1, overtime: 130},
-  {workplace: '14', shifts: 1, overtime: 140},
-  {workplace: '15', shifts: 1, overtime: 150},
-]
-
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
@@ -33,13 +9,6 @@ const CAPACITIES: ProdCapacities[] = [
 })
 export class ResultComponent implements OnInit {
   data: any;
-  displayedColumnsCapacities: string[] = [
-    'workplace',
-    'shifts',
-    'overtime'
-  ];
-
-  dataSourceCapacities = CAPACITIES;
 
   constructor(
     private resultService: ResultService,
--- src/app/result/result.module.ts
+++ src/app/result/result.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultComponent } from './result.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [ResultComponent],
-  imports: [
-    CommonModule,
-    MatTabsModule,
-    MatTableModule
-  ],
+  imports: [CommonModule],
   exports: [ResultComponent],
 })
 export class ResultModule {}
