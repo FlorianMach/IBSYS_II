747ac5dc5d113eb65246821209bd1d8d62e34eb9
wosi1018 <55805128+wosi1018@users.noreply.github.com>
2020-12-02 17:42:06 +0100

    added 2 tabs

--- src/app/product-planning/product-planning.module.ts
+++ src/app/product-planning/product-planning.module.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProductPlanningComponent } from './product-planning.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { I18nModule } from '../shared/i18n/i18n.module';
 
 @NgModule({
   declarations: [ProductPlanningComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    I18nModule
+  ],
   exports: [ProductPlanningComponent],
 })
 export class ProductPlanningModule {}
