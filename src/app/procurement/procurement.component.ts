25fb64f4f4017f36811477f147527802f90d883b
kovi1014 <kovi1014@hs-karlsruhe.de>
2020-12-04 13:01:00 +0100

    Header fix in produrement and read out initial stock

--- src/app/procurement/procurement.component.ts
+++ src/app/procurement/procurement.component.ts
@@ -13,9 +13,8 @@ export class ProcurementComponent implements OnInit {
   data: any[] = [];
   nValues: MRP2PSNS[] = []
   initialStock: any[] = []
-  p1 = 1;
-  p2 = 0;
-  p3 = 0;
+  orderN = "orderN"
+  orderE = "orderE"
 
   constructor(
     private procurementService: ProcurementService,
@@ -25,29 +24,30 @@ export class ProcurementComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    console.log('this.data...', this.initialStock)
-
     this.xmlReaderService.subscribe((data) => {
       this.data = data;
-      this.initialStock = this.data && this.data['results'] && this.data['results'].warehousestock && this.data['results'].warehousestock.article[0] && this.data['results'].warehousestock.article[0]._attributes.amount ? this.data['results'].warehousestock.article.map(x => x._attributes.amount) : 0
-      console.log('this.data...', this.initialStock)
-      console.log('this.data', this.data)
+      this.initialStock = this.data && this.data['results'] && this.data['results'].warehousestock && this.data['results'].warehousestock.article[0] && this.data['results'].warehousestock.article[0]._attributes.amount ? this.data['results'].warehousestock.article.map(x => { return { amount: x._attributes.amount, id: x._attributes.id } }) : 0
     });
     this.SharedService.gtetDataOfMrp2data().subscribe(x => {
       this.nValues = x
-      // this.data ? this.data.forEach(item => {
-      //   console.log(item)
-      //   item.grossRequirements.n1
-      // }) : ''
     })
 
   }
-  calculateGrossValue(value1, value2, value3) {
-    return value1 * this.p1 + value2 * this.p2 + value3 * this.p3
+  mapInitialStockValue(id?) {
+    if (this.initialStock && this.initialStock.length) {
+      return this.initialStock.filter(m => m.id == id).map(x => x.amount)[0]
+    }
+    else {
+      return 0
+    }
+  }
+  calculateGrossValue(value1, value2, value3, p1, p2, p3) {
+    return value1 * p1 + value2 * p2 + value3 * p3
   }
   fileName = "result.xlsx"
   downloadData() {
     let element = document.getElementById("excelTable")
+    console.log('iii', element)
     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element)
     const wb: XLSX.WorkBook = XLSX.utils.book_new()
     XLSX.utils.book_append_sheet(wb, ws, "sheet1")
--- src/app/procurement/procurement.module.ts
+++ src/app/procurement/procurement.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProcurementComponent } from './procurement.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProcurementComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [ProcurementComponent],
 })
-export class ProcurementModule {}
+export class ProcurementModule { }
