494a6289b1e16c421b2823381c7d2b6732e482c0
wosi1018 <55805128+wosi1018@users.noreply.github.com>
2020-12-12 12:50:22 +0100

    input validation in dialog + export xlm button on top

--- src/app/split-dialog/split-dialog.component.ts
+++ src/app/split-dialog/split-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {MatSliderModule} from '@angular/material/slider';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-split-dialog',
@@ -24,6 +25,11 @@ export class SplitDialogComponent {
     return value;
   }
   */
+
+  negativeValue(): boolean {
+    if(this.data.rest1 <= 0 || this.data.rest2 <= 0 || this.data.rest1 > (this.data.quantity - 1)) return true;
+    else return false;
+  }
 
   calculate(event: any) {
     this.data.rest2 = (this.data.quantity - event.target.value);
