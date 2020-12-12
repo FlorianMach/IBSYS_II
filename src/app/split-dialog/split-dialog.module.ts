87b053b58a32d4257d80d091df7ea211a202cfdf
wosi1018 <55805128+wosi1018@users.noreply.github.com>
2020-12-12 10:18:52 +0100

    added translation

--- src/app/split-dialog/split-dialog.module.ts
+++ src/app/split-dialog/split-dialog.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../shared/material/material.module';
 import { MatSliderModule } from '@angular/material/slider';
+import { I18nModule } from '../shared/i18n/i18n.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MaterialModule,
-    MatSliderModule
+    MatSliderModule,
+    I18nModule
   ]
 })
 export class SplitDialogModule { }
